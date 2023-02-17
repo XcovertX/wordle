4b4aaf315824e554038d057ab0b2d677c4df4231
James Covert <jcovert2@asu.edu>
2023-02-17 11:59:53 -0800

    guess function added

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -1,3 +1,11 @@
-export default function Guess() {
-    return <div>guess</div>
+export default function Guess({ guess, word, isGuessed }) {
+    return (
+    <div className="mb-2 grid grid-cols-5 gap-2">
+        {new Array(5).fill(0).map((_, i) => (
+            <div className="h-16 w-16 border border-gray-400 bg-black font-bold uppercase text-white flex items-center justify-center">
+                {guess[i]}
+            </div>
+        ))}
+    </div>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,12 @@ import PuzzleStore from "../stores/PuzzleStore";
 export default observer(function Home() {
   const store = useLocalObservable(() => PuzzleStore)
   return <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
-    <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">Wordle</h1>
-    <Guess word={"tests"} guess={"guess"} isGuesed={false}/>
+    <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">
+      Wordle
+    </h1>
+    {new Array(6).fill(0).map((_, i) =>  (
+      <Guess word={"tests"} guess={"guess"} isGuessed={false}/>
+    ))}
     <h1>won/loss</h1>
     <Querty />
   </div>
