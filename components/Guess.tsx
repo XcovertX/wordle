66fae9cd188bc81134a42be19b1542896fb2619d
James Covert <jcovert2@asu.edu>
2023-02-17 13:42:53 -0800

    local store works

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -1,11 +1,20 @@
 export default function Guess({ guess, word, isGuessed }) {
     return (
     <div className="mb-2 grid grid-cols-5 gap-2">
-        {new Array(5).fill(0).map((_, i) => (
-            <div className="h-16 w-16 border border-gray-400 bg-black font-bold uppercase text-white flex items-center justify-center">
-                {guess[i]}
-            </div>
-        ))}
+        {new Array(5).fill(0).map((_, i) => {
+            const bgColor = !isGuessed
+            ? 'bg-black'
+            : guess[i] === word[i]
+            ? 'bg-green-400'
+            : word.includes(guess[i])
+            ? 'bg-yellow-400'
+            : 'bg-black'
+            return(
+                <div className={`h-16 w-16 border border-gray-400 ${bgColor} font-bold uppercase text-white flex items-center justify-center`}>
+                    {guess[i]}
+                </div>
+            )
+            })}
     </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,39 @@
 import { observer, useLocalObservable } from "mobx-react-lite";
+import { useEffect } from "react";
 import Guess from "../components/Guess";
 import Querty from "../components/Querty";
 import PuzzleStore from "../stores/PuzzleStore";
 
 export default observer(function Home() {
   const store = useLocalObservable(() => PuzzleStore)
+  useEffect(() => {
+    store.init()
+    window.addEventListener('keyup', store.handleKeyup)
+      return () => {
+        window.removeEventListener('keyup', store.handleKeyup)
+      }
+  }, [])
   return <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
     <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">
       Wordle
     </h1>
-    {new Array(6).fill(0).map((_, i) =>  (
-      <Guess word={"tests"} guess={"guess"} isGuessed={false}/>
+    {store.guesses.map((_, i) =>  (
+      <Guess 
+        key={i}
+        word={store.word} 
+        guess={store.guesses[i]} 
+        isGuessed={i < store.currentGuess}/>
     ))}
-    <h1>won/loss</h1>
+    {store.won && <div>You Won!</div>}
+    {store.lost && <div>You Lost!</div>}
+    {(store.won || store.lost) && (
+      <button onClick={store.init}>
+        Play Again
+      </button>
+    )}
     <Querty />
+    word: {store.word}
+    <br/>
+    guesses: {JSON.stringify(store.guesses)}
   </div>
 })
